4a0b919b5ed0d6e7d75b45abb3c92e622fe2b19e
GonzaloNuñez <gonzalonlf@hotmail.com>
2024-07-04 13:16:00 +0200

    ejercicio hecho

---
+++ src/Greeting.jsx
@@ -0,0 +1,20 @@
+//Greeting.jsx
+import { useLanguage } from "./LanguageContext";
+
+const Greeting = () => {
+  const { language } = useLanguage();
+
+  const greetings = {
+    en: "Hello",
+    es: "Hola",
+    fr: "Bonjour",
+  };
+
+  return (
+    <>
+      <h2>{greetings[language]}</h2>
+    </>
+  );
+};
+
+export default Greeting;
